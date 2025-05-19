c6255ef9908336a43f70780466fe6e5e94da82bc
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-05-19 12:44:31 +0300

    add hugging face repo aurg

--- scripts/run_train.sh
+++ scripts/run_train.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 python train.py --train_csv data/intent_train.csv --valid_csv data/intent_valid.csv
+### With huggingface repo
+# python train.py --train_csv data/intent_train.csv --valid_csv data/intent_valid.csv --repo_id prajjwal1/bert-tiny
