d419e47e17e2d32b77d6266580b579fe5ae047f5
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2025-05-19 10:30:37 +0300

    add run and rest script

---
+++ scripts/run_infer.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python predict.py --rest    # REST API at :8000/intent
---
+++ scripts/run_train.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+python train.py --train_csv data/intent_train.csv --valid_csv data/intent_valid.csv
